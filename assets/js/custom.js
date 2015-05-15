a139084987b06dd6a941ff2f3a96a34b46dece74
David Harvin <dharvin@ufl.edu>
2015-05-14 21:54:57 -0400

    clean other normalization func

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -108,33 +108,6 @@ $('.navbar-collapse ul li a').click(function() {
 });
 
 
-/*function carouselNormalization() {
-var items = $('#carousel-slider.carousel.slide.item'), //grab all slides
-    heights = [], //create empty array to store height values
-    tallest; //create variable to make note of the tallest slide
-
-if (items.length) {
-    function normalizeHeights() {
-        items.each(function() { //add heights to array
-            heights.push($(this).height()); 
-        });
-        tallest = Math.max.apply(null, heights); //cache largest value
-        items.each(function() {
-            $(this).css('min-height',tallest + 'px');
-        });
-    };
-    normalizeHeights();
-
-    $(window).on('resize orientationchange', function () {
-        tallest = 0, heights.length = 0; //reset vars
-        items.each(function() {
-            $(this).css('min-height','0'); //reset min-height
-        }); 
-        normalizeHeights(); //run it again 
-    });
-}
-}*/
-
     setCarouselHeight('#carousel-slider');
 
     function setCarouselHeight(id)
